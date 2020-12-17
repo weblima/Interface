7afec8cba38e1468c335c9030eefefbe27a4fcb2
Rafael Lima <rafael_weblima@hotmail.com>
2020-12-17 11:49:45 -0300

    Inicio criação das classes

---
+++ Contracts/Entities/Contract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contracts.Entities {
+    class Contract {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments = new List<Installment>();
+
+        public Contract() {
+        }
+        public Contract(int number, DateTime date, double totalValue) {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+
+    }
+}
---
+++ Contracts/Entities/Installment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Contracts.Entities {
+    class Installment {
+        public DateTime DueDate { get; set; }
+        public Double Amount { get; set; }
+        public Installment() {
+
+        }
+        public Installment(DateTime dueDate, double amount) {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+    }
+}
